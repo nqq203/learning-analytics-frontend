1783bce922945d3eaaf53bd209ad47ca6064c7de
tthuyduong12 <thuyduongdinhthi2312@gmail.com>
2025-03-23 21:41:45 +0700

    feat: UI for Dashboard page

---
+++ src/components/Dashboard/constant.js
@@ -0,0 +1,46 @@
+"use client"
+
+export const avgScoreChart = [
+    { year: 2015, value: 6.5 },
+    { year: 2016, value: 8 },
+    { year: 2017, value: 6 },
+    { year: 2018, value: 9.5 },
+    { year: 2019, value: 4.8 },
+    { year: 2020, value: 7.2 },
+    { year: 2021, value: 8.3 },
+    { year: 2022, value: 8.8 },
+    { year: 2023, value: 8.8 },
+    { year: 2024, value: 9.5 },
+  ]
+  
+  export const riskStudentData = [
+    { name: "Cơ sở dữ liệu", students: 65, atRisk: 8 },
+    { name: "Trực quan hóa dữ liệu", students: 80, atRisk: 5 },
+    { name: "Hệ quản trị CSDL", students: 48, atRisk: 15 },
+    { name: "Hệ quản trị CSDL", students: 95, atRisk: 3 },
+    { name: "Hệ quản trị CSDL", students: 55, atRisk: 12 },
+  ]
+  
+  export const academicRankData = [
+    { name: "Giỏi", value: 12, percentage: "28.6%", color: "#00A3A1" },
+    { name: "Khá", value: 22, percentage: "42.9%", color: "#3182CE" },
+    { name: "Trung bình", value: 12, percentage: "28.6%", color: "#F6AD37" },
+    { name: "Yếu", value: 12, percentage: "28.6%", color: "#E53E3E" },
+  ]
+  
+  export const spendingTimeChartData = [
+    { name: "Dưới 1 giờ", value: 12, percentage: "28.6%", color: "#9F7AEA" },
+    { name: "1-3 giờ", value: 22, percentage: "42.9%", color: "#3182CE" },
+    { name: "3-5 giờ", value: 12, percentage: "28.6%", color: "#ED8936" },
+    { name: "Trên 5 giờ", value: 12, percentage: "28.6%", color: "#38A169" },
+  ]
+  
+  export const subjects = [
+    "Cơ sở dữ liệu",
+    "Hệ thống thông tin doanh nghiệp",
+    "Hệ thống thông tin doanh nghiệp",
+    "Hệ thống thông tin doanh nghiệp",
+    "Hệ thống thông tin doanh nghiệp",
+    "Hệ thống thông tin doanh nghiệp",
+    "Hệ thống thông tin doanh nghiệp",
+  ]
