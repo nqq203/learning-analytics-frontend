5618542e8889ec75a561b437e53da690b47f2624
Đoàn Nhi <nhidoan2003179@gmail.com>
2025-04-22 16:21:56 +0700

    FIX: Fix UI page reports and statistics

--- src/pages/analytics/reports-and-statistics/index.js
+++ src/pages/analytics/reports-and-statistics/index.js
@@ -1,15 +1,4 @@
 import React, { useEffect, useMemo, useState } from "react";
-<<<<<<< HEAD
-import { ActionButton, Container, Header } from "@/components/Analytics/Styles/Styles";
-import { TextField, FormControl, InputLabel, MenuItem, Select, IconButton } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search'; 
-import FilterAltIcon from '@mui/icons-material/FilterAlt'; 
-import AnalyticsTable from "@/components/Analytics/Table/Table";
-import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchClassesByLecturer } from "@/redux/thunk/analyticsThunk";
-import InputAdornment from '@mui/material/InputAdornment';
-=======
 import {
   ActionButton,
   Container,
@@ -21,39 +10,26 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  IconButton,
 } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import AnalyticsTable from "@/components/Analytics/Table/Table";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchClassesByLecturer,
   searchClasses,
-  searchStudents,
 } from "@/redux/thunk/analyticsThunk";
->>>>>>> main
+import InputAdornment from '@mui/material/InputAdornment';
 
 const ClassesList = () => {
   const { totalRecords, classes } = useSelector((state) => state.analytics);
-  console.log(classes);
   const dispatch = useDispatch();
   const [search, setSearch] = useState("");
-
-<<<<<<< HEAD
   const [filterSubject, setFilterSubject] = useState("");
   const [filterClass, setFilterClass] = useState("");
 
-  const handleSubjectChange = (event) => {
-    setFilterSubject(event.target.value);
-  };
-
-  const handleClassChange = (event) => {
-    setFilterClass(event.target.value);
-  };
-  const handleFilter = () => {
-    console.log("Lọc với Môn học:", filterSubject, "và Lớp:", filterClass);
-  };
-=======
->>>>>>> main
   const rows = useMemo(() => {
     return classes || [];
   }, [classes]);
@@ -65,35 +41,22 @@ const ClassesList = () => {
   const router = useRouter();
 
   const handleSearch = () => {
-    console.log("Searching for:", subject);
-  };
-
-  useEffect(() => {
-    const fetchClasses = async () => {
-<<<<<<< HEAD
-      await dispatch(fetchClassesByLecturer({ userId: 1, page: 1, amount: 10 }));
-=======
-      await dispatch(
-        fetchClassesByLecturer({ userId: 83, page: 1, amount: 10 })
-      );
->>>>>>> main
-    };
-    fetchClasses();
-  }, []);
-
-  const handleSearch = () => {
     dispatch(
       searchClasses({ search: search, userId: 83, page: 1, amount: 10 })
     );
   };
 
-  const columns = [
-    { id: "courseName", label: "Môn học", align: "left" },
-    { id: "className", label: "Lớp", align: "left" },
-    { id: "academicYear", label: "Khóa", align: "center" },
-    { id: "totalStudents", label: "Số sinh viên", align: "center" },
-    { id: "passRate", label: "Tỷ lệ đậu (%)", align: "center" },
-  ];
+  const handleSubjectChange = (event) => {
+    setFilterSubject(event.target.value);
+  };
+
+  const handleClassChange = (event) => {
+    setFilterClass(event.target.value);
+  };
+
+  const handleFilter = () => {
+    console.log("Lọc với Môn học:", filterSubject, "và Lớp:", filterClass);
+  };
 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
@@ -103,9 +66,25 @@ const ClassesList = () => {
     router.push(`/analytics/reports-and-statistics/${btoa(classId)}`);
   };
 
+  useEffect(() => {
+    const fetchClasses = async () => {
+      await dispatch(
+        fetchClassesByLecturer({ userId: 83, page: 1, amount: 10 })
+      );
+    };
+    fetchClasses();
+  }, []);
+
+  const columns = [
+    { id: "courseName", label: "Môn học", align: "left" },
+    { id: "className", label: "Lớp", align: "left" },
+    { id: "academicYear", label: "Khóa", align: "center" },
+    { id: "totalStudents", label: "Số sinh viên", align: "center" },
+    { id: "passRate", label: "Tỷ lệ đậu (%)", align: "center" },
+  ];
+
   return (
     <Container>
-<<<<<<< HEAD
       <Header style={{ alignItems: 'center', gap: '16px', flexWrap: 'wrap' }}>
         <div style={{ display: "flex", alignItems: "center", gap: "8px", width: "100%" }}>
           <TextField
@@ -142,7 +121,8 @@ const ClassesList = () => {
               },
             }}
           />
-           <FormControl style={{ width: "20%", minWidth: 250 }} size="small">
+
+          <FormControl style={{ width: "20%", minWidth: 250 }} size="small">
             <InputLabel>Môn học</InputLabel>
             <Select
               label="Môn học"
@@ -175,40 +155,7 @@ const ClassesList = () => {
       </Header>
 
       <div style={{ display: "flex", flexDirection: "column" }}>
-        <span style={{ paddingLeft: "20px", paddingTop: "20px", fontSize: "15px", fontWeight: "540", color: "#1976D2" }}>
-=======
-      <Header>
-        <TextField
-          placeholder="Môn học, Lớp, Khóa"
-          variant="outlined"
-          label="Tìm kiếm"
-          value={search}
-          onChange={handleSearchChange}
-          style={{ width: "90%" }}
-        />
-        <ActionButton
-          variant="contained"
-          style={{ fontWeight: "700", fontSize: "14px" }}
-          onClick={() => handleSearch()}
-        >
-          Tìm kiếm
-        </ActionButton>
-      </Header>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <span
-          style={{
-            paddingLeft: "20px",
-            paddingTop: "20px",
-            fontSize: "20px",
-            fontWeight: "700",
-          }}
-        >
->>>>>>> main
+        <span style={{ paddingLeft: "20px", paddingTop: "20px", fontSize: "20px", fontWeight: "700" }}>
           Tổng số lớp hiển thị: {totalStudents}
         </span>
         <AnalyticsTable
