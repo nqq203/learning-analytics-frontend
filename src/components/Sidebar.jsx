f911b4226a8010afe006e1d2495387b6609be4eb
tthuyduong12 <thuyduongdinhthi2312@gmail.com>
2025-05-27 14:20:19 +0700

    fix: titles of tabs in Thong Ke

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -310,7 +310,7 @@ const Sidebar = ({
                   "/analytics/reports-and-statistics"
                 )}
               >
-                BIỂU ĐỒ VÀ BÁO CÁO
+                THEO SINH VIÊN
               </NavText>
             </SubNavItem>
           </Link>
@@ -334,7 +334,7 @@ const Sidebar = ({
                   "/analytics/learning-outcome"
                 )}
               >
-                KẾT QUẢ CHI TIẾT
+                THEO LỚP
               </NavText>
             </SubNavItem>
           </Link>
@@ -350,7 +350,7 @@ const Sidebar = ({
                 $isMobile={isMobile}
                 $active={router.pathname.startsWith("/analytics/compare")}
               >
-                SO SÁNH KQ HỌC TẬP
+                THEO MÔN
               </NavText>
             </SubNavItem>
           </Link>
