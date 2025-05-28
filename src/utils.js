c7bcd962c07d64e6c8cb24a49ba35ebe21d8b6d3
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2025-05-28 13:28:50 +0700

    UPDATE: update header for dashboard

--- src/utils.js
+++ src/utils.js
@@ -18,29 +18,29 @@ export const getTabTitle = (pathname) => {
         return "BIỂU ĐỒ VÀ BÁO CÁO";
     } else if (pathname.startsWith("/analytics/learning-outcome")) {
         return "KẾT QUẢ HỌC TẬP";
-    } else if(pathname.startsWith("/analytics/compare")) {
+    } else if (pathname.startsWith("/analytics/compare")) {
         return "SO SÁNH KẾT QUẢ HỌC TẬP";
     } else if (pathname.startsWith("/predictions/fraud-detection")) {
         return "PHÁT HIỆN GIAN LẬN";
     } else if (pathname.startsWith("/predictions/predict-achievements")) {
         return "DỰ ĐOÁN THÀNH TÍCH";
-    } else if(pathname.startsWith("/alerts")) {
+    } else if (pathname.startsWith("/alerts")) {
         return "THÔNG BÁO"
-    } else if(pathname.startsWith("/data-management/file-management")) {
+    } else if (pathname.startsWith("/data-management/file-management")) {
         return "QUẢN LÝ TẬP TIN"
-    } else if(pathname.startsWith("/data-management/student-management")) {
+    } else if (pathname.startsWith("/data-management/student-management")) {
         return "QUẢN LÝ SINH VIÊN"
-    } else if(pathname.startsWith("/data-management/class-management")) {
+    } else if (pathname.startsWith("/data-management/class-management")) {
         return "QUẢN LÝ LỚP HỌC"
-    } else if(pathname.startsWith("/data-management")) {
+    } else if (pathname.startsWith("/data-management")) {
         return "QUẢN LÝ DỮ LIỆU"
-    }  
+    }
+    else if (pathname.startsWith("/dashboard")) {
+        return "DASHBOARD";
+    }
     else if (pathname.startsWith("/")) {
         return "TRANG CHỦ";
-    } 
-     else if (pathname.startsWith("/dashboard")) {
-        return "DASHBOARD";
-    } 
-    
+    }
+
     return "TIÊU ĐỀ";
 }
