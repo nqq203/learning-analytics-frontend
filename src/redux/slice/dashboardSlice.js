70d8a9c98a2f933be9b7a9d81a755ffc957d26ff
tthuyduong12 <thuyduongdinhthi2312@gmail.com>
2025-06-16 23:20:22 +0700

    feat: integrate API for dashboard page

---
+++ src/redux/slice/dashboardSlice.js
@@ -0,0 +1,102 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  fetchSummaryThunk,
+  fetchAvgScoreChartThunk,
+  fetchAcademicRankDataThunk,
+  fetchSubjectsThunk,
+  fetchDashboardCardsThunk,
+} from "../thunk/dashboardThunk";
+
+const initialState = {
+  loading: false,
+  error: null,
+  summary: null,
+  avgScoreChart: [],
+  academicRankData: [],
+  riskStudentData: [],
+  spendingTimeChartData: [],
+  subjects: [],
+  cardsData: null,
+};
+
+const dashboardSlice = createSlice({
+  name: "dashboard",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSummaryThunk.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchSummaryThunk.fulfilled, (state, action) => {
+        console.log("Payload from fetchSummaryThunk:", action.payload);
+        const {
+          academicRankData,
+          avgScoreChart,
+          riskStudentData,
+          spendingTimeChartData,
+          subjects,
+        } = action.payload ?? {};
+
+        state.academicRankData = academicRankData ?? [];
+        state.avgScoreChart = avgScoreChart ?? [];
+        state.riskStudentData = riskStudentData ?? [];
+        state.spendingTimeChartData = spendingTimeChartData ?? [];
+        state.subjects = Array.isArray(subjects) ? subjects : [];
+      })
+      .addCase(fetchDashboardCardsThunk.fulfilled, (state, action) => {
+        state.cardsData = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchDashboardCardsThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchDashboardCardsThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error =
+          action.error?.message || "Không thể tải dữ liệu thống kê.";
+      });
+
+    // Avg Score Chart
+    //   .addCase(fetchAvgScoreChartThunk.pending, (state) => {
+    //     state.loading = true;
+    //   })
+    //   .addCase(fetchAvgScoreChartThunk.fulfilled, (state, action) => {
+    //     state.loading = false;
+    //     state.avgScoreChart = action.payload;
+    //   })
+    //   .addCase(fetchAvgScoreChartThunk.rejected, (state, action) => {
+    //     state.loading = false;
+    //     state.error = action.payload;
+    //   })
+
+    //   // Academic Rank
+    //   .addCase(fetchAcademicRankDataThunk.pending, (state) => {
+    //     state.loading = true;
+    //   })
+    //   .addCase(fetchAcademicRankDataThunk.fulfilled, (state, action) => {
+    //     state.loading = false;
+    //     state.academicRankData = action.payload;
+    //   })
+    //   .addCase(fetchAcademicRankDataThunk.rejected, (state, action) => {
+    //     state.loading = false;
+    //     state.error = action.payload;
+    //   })
+
+    //   // Subjects
+    //   .addCase(fetchSubjectsThunk.pending, (state) => {
+    //     state.loading = true;
+    //   })
+    //   .addCase(fetchSubjectsThunk.fulfilled, (state, action) => {
+    //     state.loading = false;
+    //     state.subjects = action.payload;
+    //   })
+    //   .addCase(fetchSubjectsThunk.rejected, (state, action) => {
+    //     state.loading = false;
+    //     state.error = action.payload;
+    //   });
+  },
+});
+
+export default dashboardSlice.reducer;
