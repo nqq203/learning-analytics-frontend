8631edb952175f66a532fbb794a226026a1a9d44
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2025-07-23 19:55:29 +0700

    UPDATE: reverse proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // async rewrites() {
-    //     return [
-    //         {source: '/api/:path*', destination: 'https://la-app.onrender.com/api/:path*'}
-    //     ]
-    // }
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://la-app.onrender.com/api/:path*', // Backend thật
+            },
+        ];
+    },
 };
 
 export default nextConfig;
